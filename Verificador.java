98dbe8539823fe26633a8b7fb029dd93b32cc2fd
JD110225 <51008292+JD110225@users.noreply.github.com>
2019-09-12 17:55:12 -0600

    Add files via upload

---
+++ Verificador.java
@@ -0,0 +1,29 @@
+import java.lang.Math;
+public class Verificador
+{
+   public boolean esInterior(double puntoX,double puntoY,double radioCirculo){
+     boolean interior=false;
+       if(Math.pow(puntoX-1,2)+Math.pow(puntoY-1,2)<Math.pow(radioCirculo,2)){
+           interior=true;
+       }
+     return interior;
+}
+    public void circuloAcertado(double puntoX,double puntoY){
+        if(esInterior(puntoX,puntoY,0.1)){
+            System.out.println("Circulo menor");
+        }
+        else{
+            if(esInterior(puntoX,puntoY,0.5)){
+                System.out.println("Circulo medio");
+            }
+            else{
+                if(esInterior(puntoX,puntoY,1)){
+                    System.out.println("Circulo grande");
+                }
+                else{
+                    System.out.println("No hubo acierto");
+                }
+    }
+}
+}
+}
